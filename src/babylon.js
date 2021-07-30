b802aa356a0d86a747ecbfd15359d9e4fdc24d62
Gordon Stein <gstein@ltu.edu>
2021-07-30 17:57:57 -0500

    improve ground

--- src/babylon.js
+++ src/babylon.js
@@ -13,15 +13,16 @@ const resizeBabylonCanvas = function () {
     engine.resize();
 };
 
-const activateBabylon = function() {
+const activateBabylon = async function () {
     canvas = document.getElementById('3dcanvas');
     engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
     stage = world.children[0].children.find(c => c.name == 'Stage');
 
-
-    
     scene = new BABYLON.Scene(engine);
     
+    //enable Physics in the scene
+    scene.enablePhysics(new BABYLON.Vector3(0,-10,0), new BABYLON.AmmoJSPlugin());
+
     // Parameters : name, position, scene
     camera = new BABYLON.UniversalCamera('UniversalCamera', new BABYLON.Vector3(0, 1.25, 2.5), scene);
     camera.speed = 0.4;
@@ -36,13 +37,22 @@ const activateBabylon = function() {
     light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
     
     // Make ground
-    const plane = BABYLON.MeshBuilder.CreatePlane('plane', {height:20, width: 20, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
-    plane.rotate(BABYLON.Vector3.Right(), Math.PI / 2);
-    
+    var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+    ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0.5, restitution: 0.7 }, scene);
+
+    addRobot();
+
     engine.runRenderLoop(function () {
         if (stage.boundingBox().width() != canvas.width || stage.boundingBox().height() != canvas.height ) {
             resizeBabylonCanvas();
         }
+
+        // Limit camera
+        if (camera.position._y < 0.1) {
+            camera.position._y = 0.1;
+            camera.position._isDirty = true;
+        }
+
         canvas.style.left = stage.boundingBox().left() + 'px';
         canvas.style.top = stage.boundingBox().top() + 'px';
 
@@ -58,6 +68,27 @@ const activateBabylon = function() {
 const addRobot = async function () {
     let imported = await BABYLON.SceneLoader.ImportMeshAsync('', './src/', 'parallax_robot.gltf');
     let mesh = imported.meshes[1];
-    mesh.position = {...mesh.position, _isDirty: true, _y: 0.05};
+
+    var collidersVisible = true;
+    var boxCollider = BABYLON.Mesh.CreateBox('box1', 0.3, scene);
+    boxCollider.position.y = mesh.position._y;
+    boxCollider.isVisible = collidersVisible;
+    
+    // Create a physics root and add all children
+    mesh.addChild(boxCollider);
+    mesh.position.y += 1;
+
+    // Enable physics on colliders first then physics root of the mesh
+    boxCollider.physicsImpostor = new BABYLON.PhysicsImpostor(boxCollider, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
+    mesh.physicsImpostor = new BABYLON.PhysicsImpostor(mesh, BABYLON.PhysicsImpostor.NoImpostor, {  
+        mass: 5,
+        friction: 0.5,
+        restitution: 0.5,
+        nativeOptions: {
+            noSleep: true,
+            move: true
+        }
+    }, scene);
+
     return mesh;
 };
