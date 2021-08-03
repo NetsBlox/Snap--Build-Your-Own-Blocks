c5299db7069b6344eece43e39a684c27389e8aab
Brian Broll <brian.broll@gmail.com>
2021-08-03 11:00:05 -0500

    Add support for defining custom events. Closes #1266

--- src/actions.js
+++ src/actions.js
@@ -3058,13 +3058,7 @@ ActionManager.prototype.afterActionApplied = function(action) {
     // a custom block)
 
     active.onSetActive();
-    this.listeners.forEach(pair => {
-        const [source, origin] = pair;;
-        source.postMessage({
-            type: 'action',
-            data: action
-        }, origin);
-    });
+    ide.events.dispatchEvent('action', action);
 };
 
 ActionManager.prototype.onMessage = function(msg) {
