552b3439afcec2533e76d2f492fb09dc5aca81c8
Gordon Stein <gstein@ltu.edu>
2022-03-01 14:43:21 -0600

    Reset cam on room leave

--- src/roboscapesim.js
+++ src/roboscapesim.js
@@ -215,6 +215,11 @@ function leaveRoom() {
     bodyMeshes = {};
 
     updateCanvasTitle('Not connected');
+
+    // Reset camera
+    scene.activeCamera = camera;
+    camera.position = new BABYLON.Vector3(4, 10, -4);
+    camera.setTarget(new BABYLON.Vector3(0, 0, 0));
 }
 
 var assetsDir;
