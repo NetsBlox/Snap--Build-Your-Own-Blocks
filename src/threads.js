1393937c0093c49d2f91125b713481167c377661
samankittani <samankittani@gmail.com>
2026-07-15 12:32:44 -0500

    hotfix(threads): remove upstream dead code

--- src/threads.js
+++ src/threads.js
@@ -3472,50 +3472,10 @@ Process.prototype.doAsk = function (data) {
             morph => morph instanceof StagePrompterMorph
         );
         if (!activePrompter) {
-            if (data instanceof List) {
+            if (!isStage && !isHiddenSprite) {
+                rcvr.bubble(data, false, true);
+            } else if (isStage) {
                 rcvr.stopTalking();
-                this.prompter = new StagePickerMorph(data);
-                this.prompter.createItems(stage.scale);
-                leftSpace = rcvr.left() - stage.left();
-                rightSpace = stage.right() - rcvr.right();
-                if (isStage) {
-                    this.prompter.popup(
-                        stage,
-                        stage.center().subtract(
-                            this.prompter.extent().floorDivideBy(2)
-                        )
-                    );
-                } else {
-                    this.prompter.popup(
-                        stage,
-                        rightSpace > this.prompter.width() ||
-                                rightSpace >= leftSpace ?
-                            rcvr.topRight()
-                            : rcvr.topLeft().subtract(
-                                new Point(this.prompter.width(), 0)
-                            )
-                    );
-                }
-            } else {
-                if (!isStage && !isHiddenSprite) {
-                    rcvr.bubble(data, false, true);
-                } else if (isStage) {
-                    rcvr.stopTalking();
-                }
-                this.prompter = new StagePrompterMorph(
-                    isStage || isHiddenSprite ? data : null
-                );
-                if (stage.scale < 1) {
-                    this.prompter.setWidth(stage.width() - 10);
-                } else {
-                    this.prompter.setWidth(stage.dimensions.x - 20);
-                }
-                this.prompter.fixLayout();
-                this.prompter.setCenter(stage.center());
-                this.prompter.setBottom(stage.bottom() - this.prompter.border);
-                stage.add(this.prompter);
-                this.prompter.inputField.edit();
-                stage.changed();
             }
             this.prompter = new StagePrompterMorph(
                 isStage || isHiddenSprite ? data : null
@@ -3531,6 +3491,7 @@ Process.prototype.doAsk = function (data) {
             stage.add(this.prompter);
             this.prompter.inputField.edit();
             stage.changed();
+            
         }
     } else {
         if (this.prompter.isDone) {
